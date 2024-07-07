7fd44e2d1482fa823ad0af3c563279d52de86331
vdnk_d <dmitryvdvnk@gmail.com>
2024-07-07 23:32:07 +0300

    Initial Commit

---
+++ AppGuessNumber/AppGuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppGuessNumberApp.swift
+//  AppGuessNumber
+//
+//  Created by Dmitry Vdovenko on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppGuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
